f42619625c6d2b08e0a76a12dd5d778274984d80
Madhu Rao <madhurao@madhus-mbp.home>
2020-03-26 03:06:22 +0000

    first commit with edit app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload [Madhu, hell this is react app on AWS, good job by the way].
+        </p>
+        <p>
+          <h1>Hello react world on AWS</h1>
         </p>
         <a
           className="App-link"
